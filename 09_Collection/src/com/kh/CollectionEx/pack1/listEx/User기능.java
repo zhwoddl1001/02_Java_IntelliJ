f7db1480ce2a228d17b6bc6f002a086f74ed5105
zhwoddl1001 <zhwoddl0723@gmail.com>
2024-11-26 17:29:46 +0900

    Abstract interface ArrayList

---
+++ 09_Collection/src/com/kh/CollectionEx/pack1/listEx/User기능.java
@@ -0,0 +1,23 @@
+package com.kh.CollectionEx.pack1.listEx;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class User기능 {
+    //1. 저장된 사용자들을 저장할 목록 생성
+    private ArrayList<User> userList = new ArrayList<>();
+
+    //2.스캐너 사용
+    private Scanner sc = new Scanner(System.in);
+
+    //3.사용자 추가 기능 만들기!
+    public void addUser() {}
+
+    //4. 유저 목록 조회하기
+    public void allUsers() {
+        System.out.println("\n ===추가된 사용자 목록 ===");
+        //만약에 등록된 사용자가 없다면 "등록된 사용자가 없습니다." 출력
+
+        //사용자가 1명이라도 없다면 향상된 for문을 이용해서 유저들을 모두 출력
+    }
+}

2b2a08989eae7d08673498d161d31f2debe89936
zhwoddl1001 <zhwoddl0723@gmail.com>
2024-11-28 17:49:13 +0900

    Exception Io

--- 11_io/src/com/kh/example/preactice7/model/vo/StudentController.java
+++ 11_io/src/com/kh/example/preactice7/model/vo/StudentController.java
@@ -7,6 +7,11 @@ public class StudentController {
 
 
     public void printStudent(){
-        students.set("김길동","자바",);
+        students.add(new Student("김길동","자바",100));
+        students.add(new Student("박길동","디비",50));
+        students.add(new Student("이길동","화면",85));
+        students.add(new Student("정길동","서버",60));
+        students.add(new Student("홍길동","자바",20));
     }
+
 }
